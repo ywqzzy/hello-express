928920f307bcd693ccd3b607d4348f2177984d8c
ywqzzy <592838129@qq.com>
2019-05-12 21:23:22 +0800

    use template engine

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var fs = require('fs');
 var multer = require('multer')
-
 var createFolder = function(folder) {
   try{
     fs.accessSync(folder)
@@ -12,7 +11,6 @@ var createFolder = function(folder) {
 }
 var uploadFolder = './upload'
 createFolder(uploadFolder)
-
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, uploadFolder)
@@ -21,12 +19,14 @@ var storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
-
 var upload = multer({storage: storage})
-
 var app = express()
+
+app.set('view engine', 'ejs')
 var urlencodedParser = bodyParser.urlencoded({extended:false})
 var jsonParser = bodyParser.json()
+
+
 //app.use(bodyParser.urlencoded({extended:false}))
 
 
@@ -40,11 +40,29 @@ var jsonParser = bodyParser.json()
   //res.send(responseObject)
   res.json(responseObject)
 })*/
-
-
-app.get('/form', function(req, res) {
+app.get('/form1', function(req, res) {
   var form = fs.readFileSync('./form.html', {encoding: "utf8"})
   res.send(form)
+})
+
+app.get('/form/:name', function(req, res) {
+  var data = {
+    age: 29,
+    job: "haha",
+    hobbie:['coding', 'basket', 'zzy']
+  }
+  res.render('form', {data: data})
+  res.sendFile(__dirname + '/form.html')
+})
+
+app.get('/about/:name', function(req, res) {
+  var data = {
+    age: 29,
+    job: "haha",
+    hobbie:['coding', 'basket', 'zzy']
+  }
+  res.render('form', {data: data})
+  res.sendFile(__dirname + '/form.html')
 })
 
 app.post('/', urlencodedParser, function(req, res) {
@@ -56,9 +74,6 @@ app.post('/upload', upload.single('logo'), function(req, res) {
   console.dir(req.file)
   res.send({'ret_code':200})
 })
-
-
-
 //post delete
 app.listen(3000)
 console.log("listening")
